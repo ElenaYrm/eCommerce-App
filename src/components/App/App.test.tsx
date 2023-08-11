06ed9acb1bec155033235ce56ec2a447d6cabde3
Here East <nastiapiven@gmail.com>
2023-08-11 10:00:44 +0200

    feat: create basic router

--- src/components/App/App.test.tsx
+++
@@ -1,4 +0,0 @@
-test('smt', () => {
-  expect(1).toEqual(1);
-  expect(true).toBeTruthy();
-});
---
+++ src/router/Router.tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { Login } from '../pages/Login';
+import { Register } from '../pages/Register';
+import { NotFound } from '../pages/NotFound';
+
+export default function Router(): ReactElement {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
