bcdf338d947d6b726b4186cc523cdf5ef0defd70
Here East <nastiapiven@gmail.com>
2023-08-11 09:13:11 +0200

    feat: install react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './components/App/App.tsx';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
