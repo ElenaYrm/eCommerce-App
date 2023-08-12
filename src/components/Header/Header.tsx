2ab3a1b44fd93cf9967927f774c71b48e96527ff
Here East <nastiapiven@gmail.com>
2023-08-12 21:17:04 +0200

    feat: add update header state depending on auth state

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,36 @@
-import { ReactElement } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { ReactElement, MouseEvent } from 'react';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import styles from './Header.module.scss';
+import { useAuthContext } from '../../router/hooks/useAuth';
 
 export default function Header(): ReactElement {
+  const navigate = useNavigate();
+  const { isAuth, logOut } = useAuthContext();
+
+  function handleLogout(e: MouseEvent<HTMLAnchorElement>): void {
+    e.preventDefault();
+    logOut();
+    navigate('/', { replace: true });
+  }
+
   return (
     <header className={styles.header}>
       <Link to="/">Scoop</Link>
 
       <nav className={styles.header__nav}>
-        <NavLink to="/login">Login</NavLink>
-        <NavLink to="/register">Register</NavLink>
+        {isAuth ? (
+          <>
+            <Link to="/" onClick={handleLogout}>
+              Logout
+            </Link>
+            <NavLink to="/profile">Profile</NavLink>
+          </>
+        ) : (
+          <>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/register">Register</NavLink>
+          </>
+        )}
         <NavLink to="/cart">Cart</NavLink>
       </nav>
     </header>
