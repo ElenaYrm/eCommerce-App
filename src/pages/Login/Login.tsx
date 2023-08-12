ce86727b1372b14c8e3889eed359267e18cf1d0f
Here East <nastiapiven@gmail.com>
2023-08-12 21:16:32 +0200

    feat: add test login and registration

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,24 @@
 import { ReactElement } from 'react';
+import { useAuthContext } from '../../router/hooks/useAuth';
+import styles from './Login.module.scss';
+import { useNavigate } from 'react-router-dom';
+import PAGE from '../../router/constants/pages';
 
 export default function Login(): ReactElement {
+  const navigate = useNavigate();
+  const { logIn } = useAuthContext();
+
+  function handleLogin(): void {
+    logIn();
+    navigate(PAGE.home.link, { replace: true });
+  }
+
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Login</h1>
+      <button className={styles.button} onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 }
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,9 +1,24 @@
 import { ReactElement } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../../router/hooks/useAuth';
+import styles from './Register.module.scss';
+import PAGE from '../../router/constants/pages';
 
 export default function Register(): ReactElement {
+  const navigate = useNavigate();
+  const { logIn } = useAuthContext();
+
+  function handleRegister(): void {
+    logIn();
+    navigate(PAGE.home.link, { replace: true });
+  }
+
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Register</h1>
+      <button className={styles.button} onClick={handleRegister}>
+        Register
+      </button>
     </div>
   );
 }
