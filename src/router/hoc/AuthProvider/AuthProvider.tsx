59410d0c64cfa036bffaf162223e44b190cdb322
Here East <nastiapiven@gmail.com>
2023-08-13 11:31:00 +0200

    fix: change type of createContext func

--- src/router/hoc/AuthProvider/AuthProvider.tsx
+++ src/router/hoc/AuthProvider/AuthProvider.tsx
@@ -1,7 +1,7 @@
 import { ReactElement, createContext, useState } from 'react';
 import { IAuthContext, Children } from '../../types';
 
-export const AuthContext = createContext({} as IAuthContext);
+export const AuthContext = createContext<IAuthContext | null>(null);
 
 export function AuthProvider({ children }: Children): ReactElement {
   const [isAuth, setIsAuth] = useState(false);
