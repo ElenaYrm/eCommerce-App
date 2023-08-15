78df8844501a37261129f0948460533684d4683e
Here East <nastiapiven@gmail.com>
2023-08-15 02:14:38 +0200

    feat: add IInputFieldProps and ISelectFieldProps interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,3 +1,19 @@
+import { FormikValues } from 'formik';
+import { Input } from './enums';
+
 export interface IValidationErrors {
   [key: string]: string;
+}
+
+export interface InputFieldProps {
+  formik: FormikValues;
+  field: Input;
+  type: string;
+  placeholder: string;
+}
+
+export interface SelectFieldProps {
+  formik: FormikValues;
+  field: Input;
+  options: string[];
 }
