c34bb779e4f7c20e9cc03b0efb61b33e2eefe30c
Here East <nastiapiven@gmail.com>
2023-08-15 10:46:18 +0200

    refactor: delete unnecessary files

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,19 +1,3 @@
-import { FormikValues } from 'formik';
-import { Input } from './enums';
-
 export interface IValidationErrors {
   [key: string]: string;
-}
-
-export interface InputFieldProps {
-  formik: FormikValues;
-  field: Input;
-  type: string;
-  placeholder: string;
-}
-
-export interface SelectFieldProps {
-  formik: FormikValues;
-  field: Input;
-  options: string[];
 }
