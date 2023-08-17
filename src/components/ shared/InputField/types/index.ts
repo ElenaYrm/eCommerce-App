ebee4ab351efba0a06d9f9a9b235dec221a8bb48
Here East <nastiapiven@gmail.com>
2023-08-17 11:56:07 +0200

    refactor: rearrange folder structure

--- src/components/ shared/InputField/types/index.ts
+++
@@ -1,9 +0,0 @@
-import { FormikValues } from 'formik';
-import { Input } from '../../../../types/enums';
-
-export interface InputFieldProps {
-  formik: FormikValues;
-  name: Input;
-  placeholder: string;
-  className?: string;
-}
--- src/components/ shared/SelectField/types/index.ts
+++
@@ -1,9 +0,0 @@
-import { FormikValues } from 'formik';
-import { Input } from '../../../../types/enums';
-
-export interface SelectFieldProps {
-  formik: FormikValues;
-  name: Input;
-  options: string[];
-  className?: string;
-}
