f9796089ec1e59f71febce3246d077c1b6e1965d
Gribanov_German <germangribanov@mail.ru>
2023-08-18 16:44:04 +0300

    fix: check any input language

--- src/utils/validation/cityValidate.ts
+++ src/utils/validation/cityValidate.ts
@@ -2,7 +2,7 @@ import { errorMsg } from '../../constant';
 
 export function cityValidate(value: string): string {
   let error: string = '';
-  const isValidCity = /^[a-zA-Z]+$/.test(value);
+  const isValidCity = /^\p{L}+$/u.test(value);
 
   if (!value || !isValidCity) {
     error = errorMsg.city.invalid;
