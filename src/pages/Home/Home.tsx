b850b03272d249ee5b27a403306011faf85632b4
Here East <nastiapiven@gmail.com>
2023-08-19 17:07:19 +0200

    feat: style links on Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,9 @@
 import { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectIsNewUser } from '../../store/auth/selectors';
+import { PATH } from '../../router/constants/paths';
+import { Page } from '../../router/types';
 import { useAppDispatch } from '../../store/store';
 import { deleteNotice } from '../../store/auth/slice';
 
@@ -17,6 +20,16 @@ export default function Home(): ReactElement {
 
   return (
     <div className={styles.home__container}>
+      <div className={styles.content}>
+        <Link to={PATH[Page.Home]} className="active">
+          Main
+        </Link>
+        <Link to={PATH[Page.Login]}>Login</Link>
+        <Link to={PATH[Page.Register]}>Register</Link>
+        <Link to={PATH[Page.Profile]}>Profile</Link>
+        <Link to={PATH[Page.Cart]}>Cart</Link>
+      </div>
+
       {isNewUser && (
         <div>
           <span>Hello!</span>
