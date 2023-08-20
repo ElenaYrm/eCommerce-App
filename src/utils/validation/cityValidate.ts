a0fa226947611d2c7f36175de8903dc0564a330f
Here East <nastiapiven@gmail.com>
2023-08-20 12:01:22 +0200

    fix: update validate rules for First name, Last name and City

--- src/utils/validation/cityValidate.ts
+++ src/utils/validation/cityValidate.ts
@@ -2,9 +2,11 @@ import { errorMsg } from '../../constant';
 
 export function cityValidate(value: string): string {
   let error: string = '';
-  const isValidCity = /^[a-zA-Z]+$/.test(value);
+  const isValidCity = /^[a-zA-Z\s]+$/.test(value.trim());
 
-  if (!value || !isValidCity) {
+  if (!value) {
+    error = errorMsg.city.empty;
+  } else if (!isValidCity) {
     error = errorMsg.city.invalid;
   }
 
--- src/utils/validation/nameValidate.ts
+++ src/utils/validation/nameValidate.ts
@@ -2,7 +2,7 @@ import { errorMsg } from '../../constant';
 
 export function nameValidate(value: string): string {
   let error: string = '';
-  const isValidFirstName = /^[a-zA-Z]+$/.test(value);
+  const isValidFirstName = /^[a-zA-Z\s]+$/.test(value.trim());
 
   if (!value) {
     error = errorMsg.firstName.empty;
