d1966ff123899f29f6930771ee0ca54f4d2cb9c4
Elena Yaroma <yaroma.elena@gmail.com>
2023-08-20 23:41:07 +0400

    fix: change common forms validation to separate input validation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
 export * from './extractLocalUser';
 export * from './checkError';
 export * from './getMonthIndex';
-export * from './validation/validateLoginForm';
-export * from './validation/validateRegisterForm';
 export * from './getCurrentDate';
