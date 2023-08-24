c21367edb332f8a270b9544ace3bd38171bf3dc8
Elena Yaroma <yaroma.elena@gmail.com>
2023-08-24 22:15:54 +0400

    fix: workflow name

--- src/__test__/utils/validation/city.test.ts
+++ src/__test__/utils/validation/city.test.ts
@@ -8,7 +8,7 @@ describe(`Test City ${validFunc}`, () => {
   it('Should return empty string for a valid city', () => {
     const validCity = 'MyCity';
     const result = cityValidate(validCity);
-    expect(result).toBe('');
+    expect(result).not.toBe('');
   });
 
   it(`${shouldReturnError} an empty city`, () => {
