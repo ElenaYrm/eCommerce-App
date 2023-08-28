fee06025b38b7fa5564be4e644a929f2cb5870eb
Here East <nastiapiven@gmail.com>
2023-08-28 18:21:38 +0200

    fix: update fetchProductData func

--- src/services/sdk/product/methods/fetchProductData.ts
+++ src/services/sdk/product/methods/fetchProductData.ts
@@ -2,21 +2,8 @@ import { ClientResponse, Product } from '@commercetools/platform-sdk';
 import { projectKey } from '../../index.ts';
 import { authRoot } from '../../auth/roots/index.ts';
 
-async function fetchProductData(productId: string): Promise<ClientResponse<Product> | undefined> {
-  try {
-    const request = authRoot.withProjectKey({ projectKey }).products().withId({ ID: productId }).get();
-    const res = await request.execute();
-
-    if (res.statusCode !== 200) {
-      throw new Error('Product is not found');
-    }
-
-    return res;
-  } catch (err) {
-    if (err instanceof Error) {
-      throw err;
-    }
-  }
+function fetchProductData(productId: string): Promise<ClientResponse<Product>> {
+  return authRoot.withProjectKey({ projectKey }).products().withId({ ID: productId }).get().execute();
 }
 
 export default fetchProductData;
