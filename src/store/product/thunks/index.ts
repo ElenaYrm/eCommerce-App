5d40c7c227d0b28f3648fa99ed42bd4ab254d164
Here East <nastiapiven@gmail.com>
2023-08-28 18:23:22 +0200

    feat: create Product thunk

---
+++ src/store/product/thunks/index.ts
@@ -0,0 +1,35 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { IProduct } from '../../../types/interfaces';
+import { IProductSlice } from '../types';
+import { checkError } from '../../../utils';
+
+import { fetchProductData } from '../../../services/sdk/product/methods';
+import { parseProductData } from '../../../utils';
+
+export const productThunk = createAsyncThunk<
+  IProduct,
+  string,
+  {
+    state: { product: IProductSlice };
+    rejectValue: string;
+  }
+>(
+  'product/productThunk',
+  async (id, { rejectWithValue }) => {
+    try {
+      const res = await fetchProductData(id);
+      return parseProductData(res.body);
+    } catch (err: unknown) {
+      return rejectWithValue(checkError(err));
+    }
+  },
+  {
+    condition: (_, { getState }): boolean => {
+      const {
+        product: { status },
+      } = getState();
+
+      return !(status === 'loading');
+    },
+  },
+);
