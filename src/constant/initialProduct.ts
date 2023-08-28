0766ccdf163c757b237bf0ce653280923d74146d
Here East <nastiapiven@gmail.com>
2023-08-28 11:32:16 +0200

    feat: create initialProduct object

---
+++ src/constant/initialProduct.ts
@@ -0,0 +1,12 @@
+import { IProduct } from '../types/interfaces';
+
+export const initialProduct: IProduct = {
+  artist: '',
+  title: '',
+  year: '',
+  description: '',
+  images: [],
+  price: 0,
+  discountPrice: 0,
+  productId: '',
+};
