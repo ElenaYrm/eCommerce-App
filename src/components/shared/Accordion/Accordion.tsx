e99d6f5aaa95ffe862e6b413bec9027f01336857
Here East <nastiapiven@gmail.com>
2023-08-31 19:08:24 +0200

    refactor: refactor Accordion code

--- src/components/shared/Accordion/Accordion.tsx
+++ src/components/shared/Accordion/Accordion.tsx
@@ -1,13 +1,6 @@
-import { useState, ReactElement } from 'react';
-import { splitToParagraphs } from '../../../utils';
-
-import styles from './accordion.module.scss';
-import classnames from 'classnames';
-
-export interface IAccordionItem {
-  title: string;
-  content: string;
-}
+import { ReactElement } from 'react';
+import { IAccordionItem } from './AccordionItem/AccordionItem';
+import { AccordionItem } from './AccordionItem/AccordionItem';
 
 export interface IAccordionProps {
   data: IAccordionItem[];
@@ -16,30 +9,10 @@ export interface IAccordionProps {
 
 export function Accordion({ data, className }: IAccordionProps): ReactElement {
   return (
-    <ul className={classnames(styles.accordion, className)}>
+    <ul className={className}>
       {data.map((item) => (
         <AccordionItem title={item.title} content={item.content} key={item.title} />
       ))}
     </ul>
-  );
-}
-
-export function AccordionItem({ title, content }: IAccordionItem): ReactElement {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function toggleAccordion(): void {
-    setIsOpen(!isOpen);
-  }
-
-  return (
-    <li className={styles.accordion__item} onClick={toggleAccordion}>
-      <div className={styles.accordion__item_header}>
-        <h3 className={styles.title}>{title}</h3>
-        <span
-          className={classnames(styles.icon__toggle, isOpen ? styles.icon__toggle_up : styles.icon__toggle_down)}
-        ></span>
-      </div>
-      {isOpen && <div className={styles.accordion__item_content}>{splitToParagraphs(content)}</div>}
-    </li>
   );
 }
