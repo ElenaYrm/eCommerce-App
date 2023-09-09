43f8ef1812ca5177ad499a27ad34b00b8041ff39
Elena Yaroma <yaroma.elena@gmail.com>
2023-09-10 00:22:03 +0400

    feat: add cart info to components

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,9 +1,69 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
+import { useAppDispatch } from '../../store/store';
+import { deleteCartThunk, getCartThunk, updateCartThunk } from '../../store/cart/thunks';
+import { useSelector } from 'react-redux';
+import { selectCart } from '../../store/cart/selectors';
+import { selectIsAuthorized } from '../../store/auth/selectors';
+
+import styles from './cart.module.scss';
 
 export default function Cart(): ReactElement {
+  const isAuthorized = useSelector(selectIsAuthorized);
+  const cart = useSelector(selectCart);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!cart.id) {
+      dispatch(getCartThunk(isAuthorized));
+    }
+  }, [isAuthorized]);
+
+  function handleClick(event: React.MouseEvent): void {
+    event.preventDefault();
+    dispatch(deleteCartThunk({ id: cart.id, version: cart.version, isAuth: isAuthorized }));
+  }
+
   return (
-    <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div
+      style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      className={styles.cart}
+    >
       <h1>Cart</h1>
+      {cart.lineItems.length > 0 ? (
+        <ul>
+          {cart.lineItems.map((item) => (
+            <li key={item.itemId}>
+              <div>{`${item.name} ${item.quantity} ${item.price}`}</div>
+              <button
+                type="button"
+                onClick={(): void => {
+                  dispatch(
+                    updateCartThunk({
+                      id: cart.id,
+                      version: cart.version,
+                      actions: [
+                        {
+                          action: 'removeLineItem',
+                          lineItemId: item.itemId,
+                          quantity: 1,
+                        },
+                      ],
+                      isAuth: isAuthorized,
+                    }),
+                  );
+                }}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>Cart is empty</div>
+      )}
+      <button type="button" onClick={handleClick} disabled={cart.lineItems.length === 0}>
+        Clear cart
+      </button>
     </div>
   );
 }
