b1f050d7f4a3f0cdda1f6361cd6623b315e620dc
Here East <nastiapiven@gmail.com>
2023-09-09 18:34:06 +0200

    refactor: move Empty Cart view to EmptyCart component

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,26 +1,10 @@
 import { ReactElement } from 'react';
+import { EmptyCart } from './EmptyCart';
 
 import styles from './cart.module.scss';
-import { Link } from 'react-router-dom';
-import { PATH } from '../../router/constants/paths';
-import { Page } from '../../router/types';
 
 export default function Cart(): ReactElement {
   const isEmpty = true;
 
-  return (
-    <div className={styles.cart}>
-      {isEmpty && (
-        <div className={styles.cart__empty}>
-          <h2 className={styles.title}>Cart is empty (´•̥̥̥o•̥̥̥`)</h2>
-          <p className={styles.text}>
-            We highly recommend to check out Scoop’s catalog and get some amazing art for your eyes and your soul.
-          </p>
-          <Link to={PATH[Page.Catalog]} className={styles.link}>
-            To Catalog
-          </Link>
-        </div>
-      )}
-    </div>
-  );
+  return <div className={styles.cart}>{isEmpty && <EmptyCart />}</div>;
 }
