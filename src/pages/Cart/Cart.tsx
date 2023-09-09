395f3e58539bb0d7598cbc0488efd6535d856179
Here East <nastiapiven@gmail.com>
2023-09-10 00:28:54 +0200

    feat: create Cart page view

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,10 +1,27 @@
 import { ReactElement } from 'react';
-import { EmptyCart } from './EmptyCart';
+import { EmptyCart } from './EmptyMessage';
+import { Button } from '../../components/shared/Button';
+import { Total } from './Total';
+import { CartList } from './CartList';
 
 import styles from './cart.module.scss';
 
 export default function Cart(): ReactElement {
-  const isEmpty = true;
+  const isEmpty = false;
 
-  return <div className={styles.cart}>{isEmpty && <EmptyCart />}</div>;
+  return (
+    <div className={styles.cart}>
+      {isEmpty && <EmptyCart />}
+      {!isEmpty && (
+        <div className={styles.cart__container}>
+          <div className={styles.items}>
+            <h2 className={styles.items__title}>Cart(0)</h2>
+            <CartList />
+            <Button type="button" name="Clear Cart" className={styles.items__button_clear} />
+          </div>
+          <Total />
+        </div>
+      )}
+    </div>
+  );
 }
