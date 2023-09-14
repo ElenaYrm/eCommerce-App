94ebe0f8866fdef126fe0e4bdb845189769063f9
Here East <nastiapiven@gmail.com>
2023-09-14 23:14:09 +0200

    fix: update About

--- src/constant/aboutus.ts
+++ src/constant/aboutus.ts
@@ -9,7 +9,14 @@ export const studentDataTabs: IAboutDataTabs[] = [
     body: {
       textAbout:
         'We recommend that you check out the tic-tac-toe game before continuing with the tutorial. One of the features that you’ll notice is that there is a numbered list to the right of the game’s board. This list gives you a history of all of the moves that have occurred in the game, and is updated as the game progresses.',
-      recommendations: ['We recommend that you check out', 'This list gives you a history of all of the moves'],
+      recommendations: [
+        'Repository setup (Vite + React + TypeScript + Eslint + Jest)',
+        'Github actions setup (tests and deploy on pull requests and merging)',
+        'Working with the server through SDK (login/register, catalog, cart)',
+        'Store setting (auth, cart, user)',
+        'Register form implementation (addresses part) and collecting all forms together',
+        'Catalog page implementation with infinite scroll',
+      ],
     },
   },
   {
@@ -20,7 +27,14 @@ export const studentDataTabs: IAboutDataTabs[] = [
     body: {
       textAbout:
         'We recommend that you check out the tic-tac-toe game before continuing with the tutorial. One of the features that you’ll notice is that there is a numbered list to the right of the game’s board. This list gives you a history of all of the moves that have occurred in the game, and is updated as the game progresses.',
-      recommendations: ['We recommend that you check out', 'This list gives you a history of all of the moves'],
+      recommendations: [
+        'Trello board setup',
+        'Working with the server through SDK (profile)',
+        'Store setting (user)',
+        'Login form implementation',
+        'Profile page implementation with tabs',
+        'About us page implementation',
+      ],
     },
   },
   {
@@ -31,7 +45,15 @@ export const studentDataTabs: IAboutDataTabs[] = [
     body: {
       textAbout:
         'We recommend that you check out the tic-tac-toe game before continuing with the tutorial. One of the features that you’ll notice is that there is a numbered list to the right of the game’s board. This list gives you a history of all of the moves that have occurred in the game, and is updated as the game progresses.',
-      recommendations: ['We recommend that you check out', 'This list gives you a history of all of the moves'],
+      recommendations: [
+        'Working with the server through SDK (product)',
+        'Store setting (product, catalog)',
+        'Routing setup',
+        'Register form implementation (user information part) with checking password field',
+        'Product page implementation with slider',
+        'Cart page implementation',
+        'Project design',
+      ],
     },
   },
 ];
