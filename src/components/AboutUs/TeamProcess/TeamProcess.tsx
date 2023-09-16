86bf5ee84d32726eea080d64376759b7dcd5b1e2
Here East <nastiapiven@gmail.com>
2023-09-16 10:51:26 +0200

    feat: add Teamwork and Process text on About page

--- src/components/AboutUs/TeamProcess/TeamProcess.tsx
+++ src/components/AboutUs/TeamProcess/TeamProcess.tsx
@@ -1,19 +1,26 @@
-import classNames from 'classnames';
-import styles from './teamProcess.module.scss';
 import { ReactElement } from 'react';
+
+import styles from './teamProcess.module.scss';
 
 function TeamProcess(): ReactElement {
   return (
     <div className={styles.root}>
       <h3 className={styles.root__title}>Teamwork & Process</h3>
-      <span className={classNames(styles.root__paragraph, styles.root__paragraphFirst)}>
-        We recommend that you check out the tic-tac-toe game before continuing with the tutorial.{' '}
-      </span>
-      <span className={styles.root__paragraph}>
-        One of the features that you’ll notice is that there is a numbered list to the right of the game’s board. This
-        list gives you a history of all of the moves that have occurred in the game, and is updated as the game
-        progresses.
-      </span>
+      <p className={styles.root__paragraph}>
+        Hey! We are a team of 3 developers, who has been working on this project for the past 7 weeks. We did our best
+        to plan carefully, work collaboratively, execute carefully and deliver visually appealing and user-friendly
+        interface for this e-commerce solution.
+      </p>
+      <p className={styles.root__paragraph}>
+        Let's have a look at our workflow and what approaches we used to make them work for our team.
+      </p>
+      <h4 className={styles.root__subtitle}>Project Kickoff</h4>
+      <p className={styles.root__paragraph}>
+        We did this meeting right before Sprint 1 started. The team gathered to understand the project's objectives,
+        scope and requirements. We discussed tech stack, planning tools and individual thoughts and suggestions on the
+        basic process.
+      </p>
+      <p className={styles.root__paragraph}>Actually, we needed more than one kickoff.</p>
     </div>
   );
 }
