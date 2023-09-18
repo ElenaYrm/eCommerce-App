b041ee6262aa2dbd65206b737ce504ec0b5f024c
Here East <nastiapiven@gmail.com>
2023-09-18 18:10:19 +0200

    feat: add test for EmptyMessage Cart component

---
+++ src/pages/Cart/EmptyMessage/emptyMessage.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import EmptyMessage from './EmptyMessage';
+import { PATH } from '../../../router/constants/paths';
+import { Page } from '../../../router/types'
+
+describe('Cart EmptyMessage component', () => {
+  it('should render empty cart message.', () => {
+    render(
+      <MemoryRouter>
+        <EmptyMessage />
+      </MemoryRouter>,
+    );
+
+    const titleElement = screen.getByText(/Cart is empty/i);
+    const textElement = screen.getByText(/We highly recommend to check out Scoop’s catalog/i);
+
+    const linkElement = screen.getByRole('link', { name: /To Catalog/i });
+    expect(linkElement).toHaveAttribute('href', PATH[Page.Catalog]);
+
+    expect(titleElement).toBeInTheDocument();
+    expect(textElement).toBeInTheDocument();
+    expect(linkElement).toBeInTheDocument();
+  });
+});
