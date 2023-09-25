e62736d6baeac1629d3973d3442580fa8c71fa95
Here East <nastiapiven@gmail.com>
2023-09-25 22:57:04 +0200

    refactor: update ProductDetails component

--- src/pages/Product/ProductDetails/ProductDetails.tsx
+++ src/pages/Product/ProductDetails/ProductDetails.tsx
@@ -1,18 +1,20 @@
-import { ReactElement, useEffect } from 'react';
+import { ReactElement, useCallback, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { selectCart, selectCartError } from '../../../store/cart/selectors';
 import { useAppDispatch } from '../../../store/store';
 import { updateCartThunk } from '../../../store/cart/thunks';
 import { IProduct } from '../../../types/interfaces';
-import { formatPrice, splitToParagraphs } from '../../../utils';
+// import { splitToParagraphs } from '../../../utils';
 import { Button } from '../../../components/shared/Button';
-import { productAccordionData } from '../../../constant';
-import { Accordion } from '../../../components/shared/Accordion';
+// import { productAccordionData } from '../../../constant';
+// import { Accordion } from '../../../components/shared/Accordion';
 import { ErrorMessage } from '../../../components/shared/ErrorMessage';
+import { useResetError } from '../../../hooks';
+import { ProductTitle } from '../ProductTitle';
+import { ProductInfo } from '../ProductInfo';
 
 import styles from './productDetails.module.scss';
 import classnames from 'classnames';
-import { resetCartError } from '../../../store/cart/slice';
 
 interface IProductDetailsProps {
   product: IProduct;
@@ -23,23 +25,18 @@ export default function ProductDetails({ product }: IProductDetailsProps): React
   const error = useSelector(selectCartError);
 
   const { productId } = product;
-  const cartItem = cart.lineItems.find((item) => item.productId === productId);
 
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    if (error) {
-      const timer = setTimeout(() => {
-        dispatch(resetCartError());
-      }, 3000);
+  // useMemo
+  const cartItem = useMemo(() => {
+    return cart.lineItems.find((item) => item.productId === productId);
+  }, [cart.lineItems, productId]);
 
-      return () => {
-        clearTimeout(timer);
-      };
-    }
-  }, [error, dispatch]);
+  // custom hook
+  useResetError(error, dispatch, 3000);
 
-  function addToCart(): void {
+  const addToCart = useCallback(() => {
     dispatch(
       updateCartThunk({
         id: cart.id,
@@ -54,9 +51,9 @@ export default function ProductDetails({ product }: IProductDetailsProps): React
         ],
       }),
     );
-  }
+  }, [dispatch, cart.id, cart.version, productId]);
 
-  function removeCartItem(): void {
+  const removeCartItem = useCallback(() => {
     dispatch(
       updateCartThunk({
         id: cart.id,
@@ -70,13 +67,12 @@ export default function ProductDetails({ product }: IProductDetailsProps): React
         ],
       }),
     );
-  }
+  }, [dispatch, cart.id, cart.version, cartItem?.itemId, cartItem?.quantity]);
 
   return (
     <>
-      <div className={styles.product}>
-        <div className={styles.product__details}>
-          <h2 className={styles.artist}>{product.artist}</h2>
+      <div className={styles.product__details}>
+        {/* <h2 className={styles.artist}>{product.artist}</h2>
           <h2 className={styles.title}>
             {product.title}, <span>{product.year}</span>
           </h2>
@@ -85,34 +81,37 @@ export default function ProductDetails({ product }: IProductDetailsProps): React
               <span className={styles.price__discounted}>{formatPrice(product.discountPrice)}</span>
             ) : null}
             <span className={styles.price__full}>{formatPrice(product.price)}</span>
-          </div>
+          </div> */}
 
-          {!cartItem ? (
-            <Button type="button" name="Add to Cart (●ω●)ノ" className={styles.button} handleClick={addToCart} />
-          ) : (
-            <Button
-              type="button"
-              name="Remove from Cart"
-              className={classnames(styles.button, styles.button__remove)}
-              handleClick={removeCartItem}
-            />
-          )}
+        <ProductTitle product={product} />
 
-          <div className={styles.info}>
-            <div className={styles.info__description}>
-              <p className={styles.text}>{splitToParagraphs(product.description)}</p>
-              <p className={styles.text}>
-                <span>Technique:</span>
-                {product.medium}
-              </p>
-              <p className={styles.text}>
-                <span>Size:</span>
-                {product.dimensions}
-              </p>
-            </div>
+        {!cartItem ? (
+          <Button type="button" name="Add to Cart (●ω●)ノ" className={styles.button} handleClick={addToCart} />
+        ) : (
+          <Button
+            type="button"
+            name="Remove from Cart"
+            className={classnames(styles.button, styles.button__remove)}
+            handleClick={removeCartItem}
+          />
+        )}
+
+        <ProductInfo product={product} />
+
+        {/* <div className={styles.info}>
+          <div className={styles.info__description}>
+            <p className={styles.text}>{splitToParagraphs(product.description)}</p>
+            <p className={styles.text}>
+              <span>Technique:</span>
+              {product.medium}
+            </p>
+            <p className={styles.text}>
+              <span>Size:</span>
+              {product.dimensions}
+            </p>
           </div>
           <Accordion data={productAccordionData} className={styles.product__accordion} />
-        </div>
+        </div> */}
       </div>
       {error && <ErrorMessage text={'Something wrong with Cart. Try again!'} className={styles.error__message} />}
     </>
